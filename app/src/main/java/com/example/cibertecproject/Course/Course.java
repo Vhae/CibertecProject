8cf21a1f83cd4884608438b7d9dcf7200d5cfc8c
maxromeroupc <maxrichard.1989@gmail.com>
2020-03-01 14:41:16 -0500

    se adioiona la clase Course

---
+++ app/src/main/java/com/example/cibertecproject/Course/Course.java
@@ -0,0 +1,34 @@
+package com.example.cibertecproject.Course;
+
+public class Course {
+
+    private int Id_Curso;
+    private String Nombre;
+    private String Descripcion;
+
+    public int getId_Curso() {
+        return Id_Curso;
+    }
+
+    public void setId_Curso(int id_Curso) {
+        Id_Curso = id_Curso;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+
+}
