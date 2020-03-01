c6aa37d5cc423119aadf3800edd1c8d77175aabd
maxromeroupc <maxrichard.1989@gmail.com>
2020-03-01 17:19:31 -0500

    version estable con listado de cursos

--- app/src/main/java/com/example/cibertecproject/ListCoursesFragment.java
+++ app/src/main/java/com/example/cibertecproject/ListCoursesFragment.java
@@ -4,10 +4,25 @@ package com.example.cibertecproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.cibertecproject.Course.ApiCourseClient;
+import com.example.cibertecproject.Course.Course;
+import com.example.cibertecproject.Course.CourseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 /**
@@ -15,6 +30,9 @@ import android.view.ViewGroup;
  */
 public class ListCoursesFragment extends Fragment {
 
+    private RecyclerView recyCursos;
+    private CourseAdapter cursoAdapter;
+    private List<Course> lstCurso;
 
     public ListCoursesFragment() {
         // Required empty public constructor
@@ -26,7 +44,16 @@ public class ListCoursesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View rootView=inflater.inflate(R.layout.fragment_list_courses, container, false);
+
+        recyCursos = rootView.findViewById(R.id.RecyListCourses);
+        recyCursos.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        lstCurso = new ArrayList<>();
+        cursoAdapter = new CourseAdapter(lstCurso);
+        recyCursos.setAdapter(cursoAdapter);
+
+        CallService();
         return rootView;
+
     }
 
     public static ListCoursesFragment newInstance() {
@@ -36,6 +63,31 @@ public class ListCoursesFragment extends Fragment {
         ListCoursesFragment fragment = new ListCoursesFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+
+    private void CallService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.62/WcfCibertec/WCFCurso.svc/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiCourseClient ApiCourseClient = retrofit.create(ApiCourseClient.class);
+        Call<List<Course>> call = ApiCourseClient.getCursos();
+        call.enqueue(new Callback<List<Course>>() {
+            @Override
+            public void onResponse(Call<List<Course>> call, Response<List<Course>> response) {
+                List<Course> lstCourses = response.body();
+                lstCurso.addAll(lstCourses);
+                cursoAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Course>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 
 }
